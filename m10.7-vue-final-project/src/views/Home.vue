c13626e7e265149625f0163be9d5281c9e8a30ea
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-28 13:08:59 +0100

    Start visit counter

--- m10.7-vue-final-project/src/views/Home.vue
+++ m10.7-vue-final-project/src/views/Home.vue
@@ -5,6 +5,26 @@
     <h1>Home</h1>
   </header>
 
+  <section class="consulted-wrapper">
+
+    <article class="consulted-container">
+      <h3>Users consulted</h3>
+      <div class="data-container">
+        <p>Lorem, ipsum dolor sit amet</p>
+        <p><strong>6</strong></p>
+      </div>
+    </article>
+
+    <article class="consulted-container">
+      <h3>Pictures consulted</h3>
+      <div class="data-container">
+        <p>Lorem, ipsum dolor sit amet</p>
+        <p><strong>6</strong></p>
+      </div>
+    </article>
+
+  </section>
+
 </template>
 
 <!-- SCRIPT -->
